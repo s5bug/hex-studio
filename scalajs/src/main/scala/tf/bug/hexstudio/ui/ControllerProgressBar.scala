8dbc73c832269a61c30f5ada369593c0350a980e
Aly <me@s5.pm>
2025-06-22 00:47:08 -0700

    Use calico.frp.given

--- scalajs/src/main/scala/tf/bug/hexstudio/ui/ControllerProgressBar.scala
+++ scalajs/src/main/scala/tf/bug/hexstudio/ui/ControllerProgressBar.scala
@@ -2,6 +2,7 @@ package tf.bug.hexstudio.ui
 
 import calico.*
 import calico.html.io.{*, given}
+import calico.frp.given
 import calico.syntax.*
 import cats.effect.*
 import cats.syntax.all.*
@@ -12,36 +13,27 @@ import fs2.dom.HtmlElement
 object ControllerProgressBar {
 
   def apply(programState: SignallingRef[IO, ProgramUiState]): Resource[IO, HtmlElement[IO]] = {
-    SignallingRef[IO].of(0).toResource.flatMap { maxVmState =>
-      programState.discrete.flatMap(_.progress.discrete).map(_.vmStates.length - 1)
-        .foreach(maxVmState.set)
-        .compile.drain.background.flatMap { _ =>
-          div(
-            idAttr := "progress-bar",
-            input.withSelf { self =>
-              (
-                `type` := "range",
-                idAttr := "progress-bar-range",
-                minAttr := "0",
-                maxAttr <-- maxVmState.map(_.toString),
-                value <-- programState.map(_.stepSelected).map(_.toString),
-                onChange --> (_.foreach(e => self.value.get.flatMap(v => programState.update(_.copy(stepSelected = v.toInt)))))
-              )
-            },
-            input.withSelf { self =>
-              (
-                `type` := "number",
-                minAttr := "0",
-                maxAttr <-- maxVmState.map(_.toString),
-                value <-- programState.map(_.stepSelected).map(_.toString),
-                onChange --> (_.foreach(e => self.value.get.flatMap(v => programState.update(_.copy(stepSelected = v.toInt)))))
-              )
-            },
-            "/",
-            maxVmState.map(_.toString)
-          )
-        }
-    }
+    inline def pStateSignal: Signal[IO, ProgramUiState] = programState
+    
+    div(
+      idAttr := "progress-bar",
+      input.withSelf { self => (
+        `type` := "range",
+        minAttr := "0",
+        maxAttr <-- pStateSignal.flatMap(_.progress).map(_.vmStates.length - 1).map(_.toString),
+        value <-- programState.map(_.stepSelected).map(_.toString),
+        onInput --> (_.foreach(e => self.value.get.flatMap(v => programState.update(_.copy(stepSelected = v.toInt)))))
+      )},
+      input.withSelf { self => (
+        `type` := "number",
+        minAttr := "0",
+        maxAttr <-- pStateSignal.flatMap(_.progress).map(_.vmStates.length - 1).map(_.toString),
+        value <-- programState.map(_.stepSelected).map(_.toString),
+        onInput --> (_.foreach(e => self.value.get.flatMap(v => programState.update(_.copy(stepSelected = v.toInt)))))
+      )},
+      "/",
+      pStateSignal.flatMap(_.progress).map(_.vmStates.length - 1).map(_.toString)
+    )
   }
 
 }
